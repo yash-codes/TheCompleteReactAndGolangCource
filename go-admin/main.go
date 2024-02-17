127a24416ef78462bc5e716358af8622b51d7017
root <root@yash.localdomain>
2024-02-17 16:10:26 +0530

    Add RESTApi for / using fiber

--- go-admin/main.go
+++ go-admin/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/gofiber/fiber/v2"
 )
 
 
 func main() {
   fmt.Println("Hello World !")
+
+  app := fiber.New()
+  // An Api for url path "/"
+  app.Get("/", func(c *fiber.Ctx) error {
+    return c.SendString("Hello, World !")
+  })
+
+  app.Listen(":8000")
 }
